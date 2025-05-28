8f442f328ae6f66a99420d1d5ea177da07425b44
Jenny <jennywei0102@gmail.com>
2025-05-28 13:10:19 +0800

    Change DB name from 'vite_blog' to 'blog'

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/vite_blog");
+    await mongoose.connect("mongodb://localhost:27017/blog");
     console.log("🔌 Trying to connect to MongoDB...");
-
     console.log("✅ MongoDB connected");
+    console.log("🧾 BD name:", mongoose.connection.name);
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
     process.exit(1);

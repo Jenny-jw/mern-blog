d23c55bfe21bed9a64770f381012417cab676a60
Jenny <jennywei0102@gmail.com>
2025-06-04 13:36:43 +0800

    Add upload endpoint into server

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
+import uploadRouter from "./routes/upload.js";
 import connectDB from "./db.js";
 
 const app = express();
@@ -11,6 +12,9 @@ await connectDB();
 app.use(cors());
 app.use(express.json());
 app.use("/api/posts", postRoutes);
+app.use("/api/upload", uploadRouter);
+app.use("/uploads", express.static("public/uploads"));
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });

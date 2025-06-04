14dfdf9b8a041b403acabea48aa32a3022c7ef68
Jenny <jennywei0102@gmail.com>
2025-06-04 10:45:13 +0800

    Change and import font

--- frontend/vite_blog/tailwind.config.js
+++ frontend/vite_blog/tailwind.config.js
@@ -1,21 +1,33 @@
+import typography from "@tailwindcss/typography";
 /** @type {import('tailwindcss').Config} */
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  safelist: ["translate-y-0", "-translate-y-full"],
   theme: {
     extend: {
       colors: {
         lightBg: "#E6FFFD",
-        lightText: "#5E5E5E",
+        lightText: "#2e2e2e",
         lightAccent: "#FFE6E8",
         lightFooter: "#FF858E", //#FFB3B9
-        darkBg: "#5E5E5E",
+        darkBg: "#2e2e2e",
         darkText: "#E6FFFD",
+        darkButton: "#b3fff9",
       },
       fontFamily: {
+        kouzan: ["KouzanBrushFont", "cursive"],
         sourceHanSerif: ["SourceHanSerifTC", "serif"],
+        notoSerif: ["Noto Serif TC", "serif"],
+      },
+      typography: {
+        left: {
+          css: {
+            p: { textAlign: "left" },
+          },
+        },
       },
     },
   },
-  plugins: [],
+  plugins: [typography],
 };

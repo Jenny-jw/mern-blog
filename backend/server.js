0061439c755ab10710115b2d1083f400bfda6a3f
Jenny <jennywei0102@gmail.com>
2025-06-06 16:55:28 +0800

    Add routers for login and protexted page

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,12 @@ import express from "express";
 import cors from "cors";
 import postRoutes from "./routes/posts.js";
 import uploadRouter from "./routes/upload.js";
+import authRouter from "./routes/auth.js";
+import verifyToken from "./middleware/verifyToken.js";
 import connectDB from "./db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
@@ -14,6 +19,10 @@ app.use(express.json());
 app.use("/api/posts", postRoutes);
 app.use("/api/upload", uploadRouter);
 app.use("/uploads", express.static("public/uploads"));
+app.use("/api", authRouter);
+app.use("/api/protected", verifyToken, (req, res) => {
+  res.json({ message: `Hello ${req.user.username}, this is protexted.` });
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);

78c8b430e2ef61f049144aa3a0a7d62cc916d5b5
Jenny <jennywei0102@gmail.com>
2025-06-06 14:59:50 +0800

    Upload gallery (multiple picture) in newPost page

--- frontend/vite_blog/src/pages/NewPost.jsx
+++ frontend/vite_blog/src/pages/NewPost.jsx
@@ -11,6 +11,7 @@ import HorizontalRule from "@tiptap/extension-horizontal-rule";
 const NewPost = () => {
   const [title, setTitle] = useState("");
   const [tags, setTags] = useState("");
+  const [imageList, setImageList] = useState([]);
   const editor = useEditor({
     extensions: [
       StarterKit,
@@ -29,6 +30,7 @@ const NewPost = () => {
       title,
       content: editor?.getHTML(),
       tags: tags.split(",").map((tag) => tag.trim()),
+      images: imageList,
     });
     alert("Successfully added an article~ 😉");
   };
@@ -62,9 +64,57 @@ const NewPost = () => {
     };
   };
 
+  const uploadToGallery = () => {
+    const fileInput = document.createElement("input");
+    fileInput.type = "file";
+    fileInput.accept = "image/*";
+    fileInput.multiple = true;
+    fileInput.click();
+
+    fileInput.onchange = async () => {
+      const files = Array.from(fileInput.files);
+      const newImageUrls = [];
+
+      for (const file of files) {
+        const formData = new FormData();
+        formData.append("image", file);
+
+        try {
+          const res = await axios.post(
+            "http://localhost:3000/api/upload",
+            formData
+          );
+          newImageUrls.push(res.data.url);
+        } catch (err) {
+          console.error("Gallery image upload failed", err);
+        }
+      }
+
+      // 更新 gallery 圖片陣列
+      setImageList((prev) => [...prev, ...newImageUrls]);
+    };
+  };
+
   return (
     <div className="max-w-screen-md mx-auto space-y-8 p-4">
       <form onSubmit={handleSubmit} className="space-y-4">
+        <div>
+          <label className="block font-semibold mb-1">Galery 🖼️</label>
+          <div className="flex gap-2 overflow-x-auto mb-2">
+            {imageList.map((url, idx) => {
+              return (
+                <img
+                  key={idx}
+                  src={url}
+                  className="w-24 h-24 object-over rounded"
+                />
+              );
+            })}
+          </div>
+          <button type="button" className="btn" onClick={uploadToGallery}>
+            Upload images to gallery 🤩
+          </button>
+        </div>
         <input
           type="text"
           placeholder="TITLE"

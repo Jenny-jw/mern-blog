ac5956105397e03d06e0fe7c52aacb7dec3d7227
Jenny <jennywei0102@gmail.com>
2025-06-26 21:27:51 +0800

    Middleware to verift JWT with token stored in cookie

--- backend/middleware/verifyToken.js
+++ backend/middleware/verifyToken.js
@@ -4,21 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const verifyToken = (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+  const token = req.coookies.token;
 
-  if (!token) {
-    return res
-      .status(401)
-      .json({ message: "Access denied. No token provided." });
-  }
+  if (!token) return res.status(401).json({ message: "No token found" });
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
   } catch (err) {
-    return res.status(403).json({ message: "Invalid or expired token." });
+    return res.status(403).json({ message: "Invalid token." });
   }
 };
 

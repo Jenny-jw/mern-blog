8c7406679ef0f67f13019090cdcceba1af08e340
Jenny <jennywei0102@gmail.com>
2025-06-30 15:38:36 +0200

    Update axios baseURL for new backend service

--- frontend/vite_blog/src/axios.js
+++ frontend/vite_blog/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://mernblog-backend-dmdb.onrender.com/api",
+  baseURL: "https://mern-blog-10r3.onrender.com/api",
   withCredentials: true,
 });
 

acfd8ed0b819cb91ec56e56025ed54fa2d71fe87
Jenny <jennywei0102@gmail.com>
2025-07-01 13:16:10 +0200

    Change CSS of single post page

--- frontend/vite_blog/src/pages/SinglePost.jsx
+++ frontend/vite_blog/src/pages/SinglePost.jsx
@@ -83,7 +83,7 @@ const SinglePost = () => {
           {/* </div> */}
         </div>
         {/* RIGHT: BLOG CONTENT */}
-        <div className="md:w-2/3 prose-left prose-lg dark:prose-invert max-w-screen-md mx-auto m-8 pt-4 md:pt-20">
+        <div className="md:w-2/3 prose-left prose-lg dark:prose-invert max-w-screen-md mx-auto m-8 pt-2 md:pt-20">
           <h1 className="text-2xl font-bold">{post.title}</h1>
           <h2 className="text-base">
             {format(new Date(post.createAt), "yyyy / MM / dd")}

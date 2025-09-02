b6c6d5e5db10e6d7abff7312b0afbd90c5498753
Jenny <jennywei0102@gmail.com>
2025-09-02 15:41:16 +0200

    Add theme-switching logic in App.jsx

--- frontend/vite_blog/src/App.jsx
+++ frontend/vite_blog/src/App.jsx
@@ -1,4 +1,5 @@
 // Main component of the app
+import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Posts from "./pages/Posts.jsx";
@@ -14,9 +15,51 @@ import ReviewComments from "./pages/ReviewComments.jsx";
 import NotFound from "./pages/NotFound.jsx";
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(() => {
+    const theme = localStorage.getItem("theme");
+    return theme === "dark"; // false: may be light or neon; true: dark mode
+  });
+  const [neon, setNeon] = useState(() => {
+    const isNeon = localStorage.getItem("neon");
+    if (isNeon != null) return isNeon === "true";
+    return false;
+  });
+  const [preNeonMode, setPreNeonMode] = useState(darkMode);
+
+  useEffect(() => {
+    const root = window.document.documentElement;
+
+    root.classList.remove("dark", "neon", "light");
+
+    if (neon) {
+      root.classList.add("neon");
+      setPreNeonMode(darkMode);
+    } else {
+      root.classList.remove("neon");
+      root.classList.add(darkMode ? "dark" : "light");
+    }
+
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+    localStorage.setItem("neon", neon ? "true" : "false");
+  }, [darkMode, neon]);
+
   return (
-    <div className="min-h-screen flex flex-col bg-lightAccent text-lightText dark:bg-darkBg dark:text-darkText">
-      <Navbar />
+    <div
+      className={`min-h-screen flex flex-col ${
+        neon
+          ? "bg-gradient-to-tl from-[var(--blue)] to-[var(--pink)] text-[var(--text)]"
+          : "bg-lightAccent text-lightText dark:bg-darkBg dark:text-darkText"
+      }`}
+    >
+      {/* <div className="min-h-screen flex flex-col bg-lightAccent text-lightText dark:bg-darkBg dark:text-darkText bg-gradient-to-tl from-[var(--blue)] to-[var(--pink)] text-[var(--text)]"> */}
+      <Navbar
+        darkMode={darkMode}
+        setDarkMode={setDarkMode}
+        neon={neon}
+        setNeon={setNeon}
+        preNeonMode={preNeonMode}
+        setPreNeonMode={setPreNeonMode}
+      />
       <main className="flex-grow w-full max-w-screen-xl mx-auto px-4 pt-16 md:pt-20">
         <Routes>
           <Route path="/" element={<Home />} />
